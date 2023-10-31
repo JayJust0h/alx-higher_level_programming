b25a1aba5ec3eb3d0e3248fbe3436ba2a48d8f45
JayJust0h <edwinjustus11@gmail.com>
2023-10-31 05:47:02 -0700

    101-remove_char_at.py

--- 0x01-python-if_else_loops_functions/101-main.py
+++ 0x01-python-if_else_loops_functions/101-main.py
@@ -5,12 +5,4 @@ print(remove_char_at("Best School", 3))
 print(remove_char_at("Chicago", 2))
 print(remove_char_at("C is fun!", 0))
 print(remove_char_at("School", 10))
-print(remove_char_at("Python", -2))
-
-guillaume@ubuntu:~/0x01$ ./101-main.py
-Bes School
-Chcago
- is fun!
-School
-Python
-guillaume@ubuntu:~/0x01$ 
+print(remove_char_at("Python", -2)) 
